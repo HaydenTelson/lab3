0aa6811649d670c3f095b4807b5a8990b112a4af
Hayden Telson <haydentelson@wireless-169-228-109-241.ucsd.edu>
2019-01-24 10:36:57 -0800

    Finished with lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,25 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	function projectClick(e){
+		console.log("Project Clicked")
+		e.preventDefault();
+		$(this).css("background-color", "#7fff00");
+		$("p").text("Changed text");
+	}
+
+	$(".thumbnail p").click(changeText);
+	function changeText(event)	{
+		$(this).text("Changed text");
+	}
+
+
+
 }
